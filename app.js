49fc6af8111fbc4ff75745d8bac522e6ced30b7b
Tangtang1031 <1012574889@qq.com>
2023-07-13 19:04:01 +0800

    2.0

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 8081;
-
-app.use(express.static('public'));
-
-app.listen(port, () => {
-    console.log(`Server running at port ${port}`);
-});
