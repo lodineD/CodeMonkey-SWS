5a872808453d21b01433edd7e3066e01c45e42e8
Tangtang1031 <1012574889@qq.com>
2023-07-15 23:42:25 +0800

    update

--- project/app.js
+++ project/app.js
@@ -4,6 +4,10 @@ const port = 80;
 
 app.use(express.static('publish'));
 
+app.get('/publish/onemaps2.0.html', (req, res) => {
+    res.status(200).send('OK');
+})
+
 app.listen(port, () => {
     console.log(`Server running at port ${port}`);
 });
