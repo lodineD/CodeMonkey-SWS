d524704985b9494b8bfd8aa924d7764a5136133e
Tangtang1031 <1012574889@qq.com>
2023-07-17 14:55:59 +0800

    update

---
+++ myapp/src/App.vue
@@ -0,0 +1,15 @@
+<script>
+  export default {
+    data() {
+      return {
+        msg : "Hello World"
+      }
+    }
+  }
+</script>
+
+<template>
+  <h3>Welcome Page</h3>
+  <p>{{ msg }}</p>
+</template>
+
--- project/app.js
+++ project/app.js
@@ -8,6 +8,18 @@ app.get('/onemaps2.0.html', (req, res) => {
     res.status(200).send('OK');
 })
 
+app.get('/index.html', (req, res) => {
+    res.status(200).send('OK');
+})
+
+app.get('/login.html', (req, res) => {
+    res.status(200).send('OK');
+})
+
+app.get('/signup.html', (req, res) => {
+    res.status(200).send('OK');
+})
+
 app.listen(port, () => {
     console.log(`Server running at port ${port}`);
 });
