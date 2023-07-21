d9f273c8910ad8b8f4bd4ae81430ed5e785e96e6
Tangtang1031 <1012574889@qq.com>
2023-07-21 22:09:53 +0800

    update

--- work/call_db.js
+++ work/call_db.js
@@ -26,41 +26,39 @@ var yellowIcon = new L.Icon({
     shadowSize: [41, 41]
 });
 
-
 function fetchData() {
     axios.get("/get-data")
         .then(response => {
             const data = response.data;
             // 全局变量，用于传送值
-            window.processData = data;
-
-            data.forEach((data) => {
-                const { latitude, longitude, car_num } = data;
-                var num = car_num * 2;
-                console.log("Latitude:", latitude);
-                console.log("Longitude:", longitude);
-                console.log("Car Number:", num);
-
-                // 显示标记点，并弹出窗口
-                if (num > 9) {
-                    var point = L.marker([latitude, longitude], { icon: RedIcon }).addTo(map);
-                    point.bindPopup(`Car Number: ${num}`);
-                }
-                else if (num > 5) {
-                    var point = L.marker([latitude, longitude], { icon: yellowIcon }).addTo(map);
-                    point.bindPopup(`Car Number: ${num}`);
-                }
-                else {
-                    var point = L.marker([latitude, longitude], { icon: greenIcon }).addTo(map);
-                    point.bindPopup(`Car Number: ${num}`);
-                }
-
-            });
+            window.processData = data;            
         })
-
         .catch(error => {
             console.log("Error:", error);
         });
+}
+
+function showData() {
+    const data = window.processData;
+    data.forEach((data) => {
+        const { latitude, longitude, car_num } = data;
+        var num = car_num * 2;
+        console.log("Latitude:", latitude);
+        console.log("Longitude:", longitude);
+        console.log("Car Number:", num);
+
+        // 显示标记点，并弹出窗口
+        if (num > 9) {
+            var point = L.marker([latitude, longitude], { icon: RedIcon }).addTo(map);
+            point.bindPopup(`Car Number: ${num}`);
+        } else if (num > 5) {
+            var point = L.marker([latitude, longitude], { icon: yellowIcon }).addTo(map);
+            point.bindPopup(`Car Number: ${num}`);
+        } else {
+            var point = L.marker([latitude, longitude], { icon: greenIcon }).addTo(map);
+            point.bindPopup(`Car Number: ${num}`);
+        }
+    });
 }
 
 // 初始加载数据
