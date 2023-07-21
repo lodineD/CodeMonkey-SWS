5b486da40194174e8be8c4a614a4d874534e60de
lodineD <599125397@qq.com>
2023-07-21 12:03:17 +0800

    upate

--- work/app.js
+++ work/app.js
@@ -2,35 +2,45 @@ const mysql = require("mysql");
 const express = require("express");
 const app = express();
 
+let connection = null;
 
-let connection = mysql.createConnection({
-    host: 'my-database-01.cgt9kgv3zopt.us-east-1.rds.amazonaws.com',
-    port: 3306,
-    user: 'admin',
-    password: 'adminpassword',
-    database: 'WebAppDatabase',
-});
+function database() {
+
+    connection = mysql.createConnection({
+        host: 'my-database-01.cgt9kgv3zopt.us-east-1.rds.amazonaws.com',
+        port: 3306,
+        user: 'admin',
+        password: 'adminpassword',
+        database: 'WebAppDatabase',
+    });
 
 
-connection.connect((error) => {
-    if (error) {
-        console.error("connect failed: " + error.stack);
-        return;
-    }
-    console.log("connect success!");
-});
-
-app.get("/get-data", (req, res) => {
-    connection.query("SELECT latitude, longitude, car_num FROM car", (error, results) => {
+    connection.connect((error) => {
         if (error) {
-            console.log("select failed: " + error.message);
-            res.status(500).json({ error: "Failed to fetch data from the database" });
+            console.error("connect failed: " + error.stack);
             return;
         }
-
-        res.json(results);// 发送后客户端
+        console.log("connect success!");
     });
-});
+
+    app.get("/get-data", (req, res) => {
+        connection.query("SELECT latitude, longitude, car_num FROM car", (error, results) => {
+            if (error) {
+                console.log("select failed: " + error.message);
+                res.status(500).json({ error: "Failed to fetch data from the database" });
+                return;
+            }
+
+            res.json(results);// 发送后客户端
+        });
+    });
+
+    connection.end();
+}
+
+database();
+
+setInterval(database, 10000);
 
 app.get('/README.html', (req, res) => {
     res.status(200).send('0K');
